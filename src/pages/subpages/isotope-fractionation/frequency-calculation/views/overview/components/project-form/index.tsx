1f067759c8c79fb404a85e2e971b4d8fb908e16e
wangjunlin03 <wangjunlin03@meituan.com>
2024-09-15 20:34:09 +0800

    fix: 测试打包

--- src/pages/subpages/isotope-fractionation/frequency-calculation/views/overview/components/project-form/index.tsx
+++ src/pages/subpages/isotope-fractionation/frequency-calculation/views/overview/components/project-form/index.tsx
@@ -88,6 +88,11 @@ export default function ProjectForm(props: IProps) {
             </Form.Item>
           </Col>
           <Col span={singleFormItem ? 24 : 12}>
+            <Form.Item label="计算元素性质" name="calculationSe" rules={[...get(rules, 'calculationService') || [], { required, message: '计算服务不允许为空' }]} extra="建议全选">
+              <CascaderPro options={CalculationServiesOptions} showCheckedStrategy="SHOW_CHILD" placeholder="请选择计算服务" allowClear />
+            </Form.Item>
+          </Col>
+          <Col span={singleFormItem ? 24 : 12}>
             <Form.Item label="计算元素" name="calculationElement" rules={[...get(rules, 'calculationElement') || [], { required, message: '计算元素不允许为空' }]}>
               <Select
                 showSearch
